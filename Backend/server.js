ee76cca86689f43e89a78834b6680fa54073a6c5
KartikeyGupta11 <g.kartikey.1104@gmail.com>
2026-05-17 19:16:34 +0530

    Replace Nodemailer with resend to send mails

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,8 @@
 import dotenv from "dotenv";
+dotenv.config();
 import { connectDB } from "./config/db.js";
 import app from "./app.js";
 
-dotenv.config();
 connectDB();
 
 const PORT = process.env.PORT || 5000;
--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -1,26 +1,52 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
+
+// export const sendEmail = async ({ to, subject, text, html }) => {
+//   try {
+//     const transporter = nodemailer.createTransport({
+//       host: "smtp.gmail.com",
+//       port: 587,
+//       secure: false,
+//       auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//       },
+//       connectionTimeout: 10000,
+//     });
+
+//     await transporter.verify();
+
+//     await transporter.sendMail({
+//       from: `"Spark Parking" <${process.env.EMAIL_USER}>`,
+//       to,
+//       subject,
+//       text,
+//       html,
+//     });
+//   } catch (error) {
+//     console.error("Email Error:", error);
+//   }
+// };
+
+import { Resend } from "resend";
+import dotenv from "dotenv";
+dotenv.config();
+
+// console.log("resend: ", process.env.RESEND_API_KEY);
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async ({ to, subject, text, html }) => {
   try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-      connectionTimeout: 10000,
-    });
-
-    await transporter.sendMail({
-      from: `"Spark Parking" <${process.env.EMAIL_USER}>`,
+    const response = await resend.emails.send({
+      from: "Spark Parking <onboarding@resend.dev>",
       to,
       subject,
       text,
       html,
     });
+
+    console.log("Email sent:", response);
   } catch (error) {
-    console.error("Email Error:", error.message);
+    console.error("Email Error:", error);
   }
 };
